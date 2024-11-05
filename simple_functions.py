852601947b82eae0b4cd9bdc035deec76fb06c2c
santhoshsubba-ta <santhosh.subba@tigeranalytics.com>
2024-11-05 12:09:06 +0530

    ENH: add function definitions for double_number and square_number

--- simple_functions.py
+++ simple_functions.py
@@ -1,16 +1,14 @@
 # Custom python functions
 
 def double_number(a):
-    return a+a
+    print(f'value before double_number(): {a}')
+    result = a+a
+    print(f'value after double_number(): {result}')
+    return result
 def square_number(a):
-    return a*a
+    print(f'value before square_number(): {a}')
+    result = a*a
+    print(f'value after square_number(): {result}')
+    return result
 
 
-a = 5
-print(f'value before double_number(): {a}')
-result = double_number(a)
-print(f'value after double_number(): {result}')
-
-print(f'value before square_number(): {a}')
-result = square_number(a)
-print(f'value after square_number(): {result}')
